4cd2f49786654419b579ff4bcb54fdd1887809e4
yulamala99 <lotooleh@ukr.net>
2023-01-13 16:51:46 +0200

    project-vue

---
+++ project-vue/src/components/ComponentMain.vue
@@ -0,0 +1,20 @@
+ <template>
+    <main class="main">
+      <div class="main-wrapper">
+        <router-view/>
+      </div>
+    </main>
+ </template>
+  
+  <script>
+  export default {
+    name: 'ComponentMain',
+    components: {
+    }
+  }
+  </script>
+  
+  <style>
+    @import "../../public/src/css/main.min.css";
+  </style>
+  
